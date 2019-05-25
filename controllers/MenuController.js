96f5b656be536bfd5a2ef00acafe73d7ff35ea9c
rosshopkins2006 <rosshopkins2006@gmail.com>
2019-05-25 15:02:25 -0400

    added date function

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,6 +9,7 @@ module.exports = class MenuController {
         message: "Please choose from an option below: ",
         choices: [
           "Add new contact",
+          "Get date",
           "Exit"
         ]
       }
@@ -19,12 +20,15 @@ module.exports = class MenuController {
   main(){
     console.log("Welcome to AddressBloc!");
     inquirer.prompt(this.mainMenuQuestions).then((response) => {
-      switch(response.mainMenuChoice){
+      switch(response.mainMenuQuestions){
         case "Add new contact":
           this.addContact();
           break;
         case "Exit":
           this.exit();
+          break;
+        case "Get date":
+          this.getDate();
         default:
           console.log("Invalid input");
           this.main();
@@ -42,7 +46,22 @@ module.exports = class MenuController {
   addContact(){
     this.clear();
     console.log('addContact called');
-    this.maine();
+    this.main();
+  }
+
+  getDate(){
+    this.clear();
+    var date = new Date();
+    var hours = date.getHours();
+    var days = date.getDay();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = date + ' ' + hours + ':' + minutes + ' ' + ampm;
+    console.log(strTime);
+    this.main();
   }
 
   exit(){
